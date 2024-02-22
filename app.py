ed3db023440ccb26d155ff485a7b79f5aae658ed
Nasiebande <nikitaesther401@gmail.com>
2024-02-22 13:34:18 +0300

    templates

--- app.py
+++ app.py
@@ -13,6 +13,16 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/comments/')
+def comments():
+    comments = ['This is the first comment.',
+                'This is the second comment.',
+                'This is the third comment.',
+                'This is the fourth comment.'
+                ]
+
+    return render_template('comments.html', comments=comments)
+
 @app.route('/capitalize/<word>/')
 def capitalize(word):
     return '<h1>{}</h1>'.format(escape(word.capitalize()))
