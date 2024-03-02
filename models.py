db5e3234561dc3b99a3643cedbb0ccff773bfe7f
Nasiebande <nikitaesther401@gmail.com>
2024-03-02 18:46:47 +0300

    updates

--- models.py
+++ models.py
@@ -1,9 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
